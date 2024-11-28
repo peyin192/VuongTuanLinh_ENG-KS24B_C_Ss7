f0bbd7f0354a8db80684cf4ea190352c9c7bbf43
peyin192 <vuonglinh190206.qv@gmail.com>
2024-11-28 08:29:00 +0700

    Upload ex07.cpp

--- ex07.cpp
+++ ex07.cpp
@@ -1,24 +1,21 @@
 #include <stdio.h>
 int main(){
-	int a=0, b, count=0, i=0;
+	int n;
     printf("Nhap phan tu cua mang: ");
-    scanf("%d", &a);
-    int num[a];
-    printf("Nhap phan tu cua mang: ");
-    do{
-        while(i!=a){
-            printf("Nhap phan tu cua mang: ");
-            scanf("%d", &b);
-                if(b%2!=0){
-                    num[i]=b;
-                    i++;
-                    count++;
+    scanf("%d", &n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+    
+        while(1){
+            printf("Nhap phan tu thu %d cua mang: ",i+1);
+            scanf("%d", &arr[i]);
+                if(arr[i]%2!=0){
+                    break;
                 }
+		else{
+			printf("Nhap lai: );
         }
         
-    }while(count!=a);
-    for(int i=0;i<a;i++){
-        printf("num[%d] = %d\n", i, num[i]);
     }
     return 0;
 }
