4c68ed0d77ea4cf2ae7193004fec37c8e17d1f1b
peyin192 <vuonglinh190206.qv@gmail.com>
2024-12-04 20:50:11 +0700

    Update ex08.cpp

--- ex08.cpp
+++ ex08.cpp
@@ -1,7 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 int main(){
-	printf("Hay nhap so hang va cot")
-	
-	printf("So hang: ");
-	scanf("%d")
+    int row, col;
+    printf("moi ban nhap vao so hang ");
+    scanf("%d", &row);
+    printf("moi ban nhap vao so cot ");
+    scanf("%d", &col);
+    int arr[row][col];
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            printf("moi ban nhap phan tu trong hang %d cot %d:", i+1, j+1);
+            scanf("%d", &arr[i][j]);
+        }
+    }
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            //printf(" phan tu hang thu %d cot thu %d la : %d\n",i+1, j+1, arr[i][j]);
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
